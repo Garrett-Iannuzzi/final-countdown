1c6e7f937884465fba4dda273bf56831207f56d0
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-03-02 17:48:10 -0700

    Add error handling for the fectch in WordContainer

--- src/WordContainer/WordContainer.svelte
+++ src/WordContainer/WordContainer.svelte
@@ -4,11 +4,14 @@
   const url = '01672bcc-913a-4964-b9c5-2f4cafa8ca78';
   export let word;
   export let searchResults;
+  let isLoading = ''
 
   const findSynonyms = (async (e) => {
     word = e.detail;
+    isLoading = '...loading';
     const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${url}`)
     const data = await response.json();
+    isLoading = ''
     const fullSynonyms = data.map(object => {
       return object.meta.syns
     })
@@ -26,9 +29,13 @@
 
 <section >  
   <h2>SYNONYMS FOR: <span>{word}</span></h2>
-    {#each searchResults as secondWord}
-      <WordBtn on:getNextSynonym={findSynonyms} secondWord={secondWord}/>
-    {/each}
+    {#if isLoading}
+      <p class='p-loading'>{isLoading}</p>
+    {:else}
+      {#each searchResults as secondWord}
+        <WordBtn on:getNextSynonym={findSynonyms} secondWord={secondWord}/>
+      {/each}
+    {/if}
 </section>
 
 <style>
@@ -49,6 +56,10 @@
     padding-bottom: .5em;
     text-transform: uppercase;
     width: 98%;
+  }
+
+  .p-loading {
+    font-size: 4em;
   }
 
   span {
