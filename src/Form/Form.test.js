08ae79343fe3f8db7acfd971487092d56f166ca8
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-03-03 17:21:06 -0700

    Clean up form.test by removing imports

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -2,6 +2,7 @@ import Form from './Form.svelte';
 import '@testing-library/jest-dom/';
 import { render } from '@testing-library/svelte';
 
+
 test('shows Form with a btn when rendered', () => {
   const { getByText } = render(Form);
   const button = getByText('Click Me');
